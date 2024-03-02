65be6eb8f8dd007b257554ed9ad0668ebdcafdd8
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-03-02 11:16:53 +0530

    Sort the games

--- src/services/imageUrl.ts
+++ src/services/imageUrl.ts
@@ -1,6 +1,7 @@
 // it would be nice to provide. width and height as an argument. But we cannot do that Because backend is not setup in
 // that way.
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return "";
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return `${url.slice(0, index)}crop/600/400/${url.slice(index)}`;
