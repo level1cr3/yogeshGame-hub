5e78311563bb2dbc1d0a44f88f0d59d894bb0155
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-03-03 07:29:39 +0530

    Implement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,25 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+type Props = {
+  onSearch: (searchText: string) => void;
+};
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} width="60px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
 };
 
 export default NavBar;
+
+// searchInput is not the direct child of our 'app component'. It is inside the navbar So we are passing function from app component to navbar and then
+// from navbar to the searchInput.
+// But this can be a issue if we have a deeply nested structure ?
+// > we learn how to handle issue like this in the next part.
