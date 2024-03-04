ac62f16cd9749b9bab48dd305fb1ebc628f1af53
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-03-04 19:15:51 +0530

    Fix build error and increase number of card skeletons

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,10 @@
 import platforms from "../data/platforms";
 
-type Platform = {
-  id: number;
-  name: string;
-  slug: string;
-};
+// type Platform = {
+//   id: number;
+//   name: string;
+//   slug: string;
+// };
 
 // const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
