9a117ed68dec78d8524b411c3a20ded73c6c6f23
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-03-04 18:58:16 +0530

    Shiping static data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import genres from "../data/genres";
 
 export type Genre = {
   id: number;
@@ -6,6 +6,7 @@ export type Genre = {
   image_background: string;
 };
 
-const useGenres = () => useData<Genre>("/genres");
+// const useGenres = () => useData<Genre>("/genres");// incase we decide to get data from server.
+const useGenres = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenres;
