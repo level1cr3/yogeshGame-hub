6fbe9316e61393dc926aa4cbe14371310d02343e
Yogesh mehra <yogeshwebdev1@gmail.com>
2024-03-14 19:55:16 +0530

    refactor: move platform type to usePlatform hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,12 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import apiClient from "../services/apiClient";
 import { FetchResponse } from "../services/apiClient";
-
-export type Platform = {
-  id: number;
-  name: string;
-  slug: string;
-};
+import { Platform } from "./usePlatforms";
 
 export type Game = {
   id: number;
